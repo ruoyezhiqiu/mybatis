59e16ea046724a6476379cbf897f8e602baa824a
ruoyezhiqiu <18380231337@163.com>
2016-10-31 14:16:20 +0800

    update

---
+++ src/com/wushengde/mybatis/test/Test.java
@@ -0,0 +1,25 @@
+package com.wushengde.mybatis.test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Test {
+	public static void main(String[] args) throws IOException {
+		 String resource = "conf.xml";
+		 //加载mybatis 的配置文件（它也加载关联的映射文件）
+		 InputStream isInputStream=Test.class.getClassLoader().getResourceAsStream(resource);
+		//构建sqlSession 的工厂
+		 SqlSessionFactory sessionFactory=new SqlSessionFactoryBuilder().build(isInputStream);
+		//创建能执行映射文件中sql 的sqlSession
+		 SqlSession session=sessionFactory.openSession();
+		//映射sql 的标识字符串
+		 String statement="com.wushengde.mybatis.test.userMapper.getUser";
+		//执行查询返回一个唯一user 对象的sql
+		 User user=session.selectOne(statement,1);
+		 System.out.println(user);
+	}
+}
